2037ac0caf9f9635e38acfcc781f61c92ca7bf4f
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2022-04-17 14:30:30 -0500

    fix: fixes for deployment

--- src/components/HeroSection/index.tsx
+++ src/components/HeroSection/index.tsx
@@ -9,7 +9,7 @@ const HeroSection = () => {
         <H1 color='#E2D21C'>Bienvenido a DogeVet</H1>
         <H5 color='#fff'>La plataforma ideal para el cuidado de mascotas</H5>
         <br />
-        <LinkButton to='/login' bgColor='primary' size='lg'>
+        <LinkButton to='/login' bgcolor='primary' size='lg'>
           Empezar &rArr;
         </LinkButton>
       </HeroContent>
