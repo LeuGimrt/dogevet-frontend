904eb003be99712ca88ec6911c5f3683c263d624
Miguel Rodriguez <miguel.rodriguez18@unmsm.edu.pe>
2022-04-17 14:25:03 -0500

    feat: add env variables for deploy

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -2,12 +2,12 @@ import { FirebaseOptions, initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig: FirebaseOptions = {
-  apiKey: "AIzaSyCf_5sWGl_lAPIRHkHxOc2lge_ECoHnjiA",
-  authDomain: "dogevet-c4861.firebaseapp.com",
-  projectId: "dogevet-c4861",
-  storageBucket: "dogevet-c4861.appspot.com",
-  messagingSenderId: "766631140245",
-  appId: "1:766631140245:web:2e2dfa35c95fe26be6b49c",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
 export const app = initializeApp(firebaseConfig);
